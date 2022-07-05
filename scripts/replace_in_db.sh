c30a168e0a0dea8fa66a07c98398adb45dd45a1a
Simon Müller <simon.d.mueller@gmail.com>
2022-07-05 10:18:23 +0200

    Update replace_in_db.sh

--- scripts/replace_in_db.sh
+++ scripts/replace_in_db.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 
 URL_REGEX='(https?|ftp|file|http)://[-[:alnum:]\+&@#/%?=~_|!:,.;]*[-[:alnum:]\+&@#/%=~_|]'
-FILE_NAME=$(pwd)/$1
-NEW_FILE_NAME=$(pwd)/migrated.$1
-CURRENT_URL=$2
-NEW_URL=$3
 
-if [ ! -f $FILE_NAME ]
+echo -en  "Enter the filename:\n"
+read FILE_NAME
+
+NEW_FILE_NAME=migrated.$FILE_NAME
+
+if [ ! -f $FILE_NAME ] || [ -z $FILE_NAME ]
 then
     echo "The source sql file (first argument) $FILE_NAME does not exist"
     exit 127;
 fi
+
+echo -en  "Enter the current url:\n"
+read CURRENT_URL
 
 if ! [[ $CURRENT_URL =~ $URL_REGEX ]]
 then
     echo "The current url $CURRENT_URL (second argument) is not an valid URL"
     exit 127;
 fi
+
+echo -en  "Enter the new url:\n"
+read NEW_URL
+
 if ! [[ $NEW_URL =~ $URL_REGEX ]]
 then
-    echo "The new url  $NEW_URL (third argument) is not an valid URL"
+    echo "The new url $NEW_URL (third argument) is not an valid URL"
     exit 127;
 fi
 
