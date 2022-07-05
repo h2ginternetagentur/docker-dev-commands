f914c0c36f5ac0ec04a0c0d5e970c4cc4090d5e6
Simon Müller <simon.d.mueller@gmail.com>
2022-07-05 10:27:14 +0200

    Create remote_execute_replace.sh

---
+++ scripts/remote_execute_replace.sh
@@ -0,0 +1,1 @@
+ wget -q https://raw.githubusercontent.com/h2ginternetagentur/docker-dev-commands/main/scripts/replace_in_db.sh -O /tmp/replace && chmod ug+x /tmp/replace && /tmp/replace && rm  /tmp/replace
